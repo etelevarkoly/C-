0d86781bee9eb895b3ffa827603f35f2fff42716
Etele Várkoly <47284757+etelevarkoly@users.noreply.github.com>
2024-06-07 18:35:54 +0200

    Add files via upload

---
+++ RANDOM_CLASSES/MATH/Calculator.cs
@@ -0,0 +1,13 @@
+// ez szintén a CSharpFundamentals NAMESPACE-hez tartozik, 
+// de annak egy child Namespace-jéhez
+
+namespace CSharpFundamentals.Math 
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
